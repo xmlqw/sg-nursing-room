93207cc7532e15781331a31ff8961319638ad860
xml <lum.xinmun@gmail.com>
2023-11-29 10:49:14 +0800

    Reverted back

--- server.js
+++ server.js
@@ -33,8 +33,8 @@ void client.db("Cluster0")
 
 // Express Routes
 app.get('/', async (req, res) => {
-  //res.send("Welcome to Nursing Room");
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.send("Welcome to Nursing Room");
+  // res.sendFile(path.join(__dirname, 'index.html'));
   // Render index.html page
 });
 
